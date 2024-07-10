fb3503fb0762510f2a4f0133da17832935491c07
briskaputraa <briskaananda103@gmail.com>
2024-07-10 13:28:55 +0700

    New Theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
-import { BackgroundBoxesDemo } from "./components/ui/BackgroundDemo";
+import { FloatingNavDemo } from "./components/ui/FloatingNavDemo";
+import { GridBackgroundDemo } from "./components/ui/GridBackground";
 import { IconCloudDemo } from "./components/ui/IconCloudDemo";
-import { NavbarDemo } from "./components/ui/NavbarDemo";
+
 
 
 export default function Home() {
   return (
    <div className="h-fit">
 
-    <NavbarDemo/>
-    <BackgroundBoxesDemo/>
+    <GridBackgroundDemo/>
+    <FloatingNavDemo/>
     <IconCloudDemo/>
 
 
