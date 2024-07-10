1414c2d174bb3e6e7989cb972e0973f1c9ff4b5a
briskaputraa <briskaananda103@gmail.com>
2024-07-10 11:51:16 +0700

    new background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import { BackgroundBoxesDemo } from "./components/ui/BackgroundDemo";
 import { IconCloudDemo } from "./components/ui/IconCloudDemo";
+import NavbarPortfolio from "./components/ui/Navbar";
 import { NavbarDemo } from "./components/ui/NavbarDemo";
+
 
 export default function Home() {
   return (
    <div className="h-fit">
 
     <NavbarDemo/>
+    <NavbarPortfolio/>
     <BackgroundBoxesDemo/>
     <IconCloudDemo/>
 
