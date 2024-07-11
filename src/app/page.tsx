8b2f5087f987eb2e43ffffffa6137df000692067
briskaputraa <briskaananda103@gmail.com>
2024-07-11 13:32:01 +0700

    Second Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import HeroSection from "./components/Page/HeroSection";
 import { DockDemo } from "./components/ui/DockDemo";
+import { HeroParallaxDemo } from "./components/ui/HeroParallaxDemo";
 
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
   
   
   <HeroSection/>
+  <HeroParallaxDemo/>
   <DockDemo/>
 
     </div>
