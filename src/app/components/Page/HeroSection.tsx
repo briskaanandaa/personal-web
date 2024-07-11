d594638c56567a039f390ccf31566089fe931767
briskaputraa <briskaananda103@gmail.com>
2024-07-11 12:50:49 +0700

    New Position

--- src/app/components/Page/HeroSection.tsx
+++ src/app/components/Page/HeroSection.tsx
@@ -2,7 +2,7 @@ import { SpotlightPreview } from "../ui/HeroSpotlightDemo"
 
 const HeroSection = () => {
   return (
-    <div className="h-fit w-full flex flex-col">
+    <div className="h-[100vh] w-full flex flex-col">
     
     <SpotlightPreview/>
     
--- src/app/components/ui/HeroSpotlightDemo.tsx
+++ src/app/components/ui/HeroSpotlightDemo.tsx
@@ -5,20 +5,19 @@ import { IconCloudDemo } from "./IconCloudDemo";
 
 export function SpotlightPreview() {
   return (
-    <div className="h-[100vh] w-full flex items-center justify-center bg-slate-900 antialiased bg-grid-white/[0.02] relative overflow-hidden">
+    <div className="h-[100vh] w-full flex items-start justify-start bg-slate-900 antialiased bg-grid-white/[0.02] relative overflow-hidden">
       <Spotlight
         className="top-40 left-0 md:left-60 md:-top-20"
         fill="white"
       />
-      <div className="h-full md:p-10 max-w-7xl relative z-10 items-center flex flex-col md:flex-row justify-center w-full">
+      <div className="h-full md:p-10 max-w-7xl relative z-10 items-center flex flex-col md:flex-row  w-full">
       
-      <div className="flex flex-col p-10">
+      <div className="flex flex-col py-6 px-10">
       <GradualSpacingDemo/>
       <BlurInDemo/>
       </div>
 
       <IconCloudDemo/>
-
       </div>
     </div>
   );
