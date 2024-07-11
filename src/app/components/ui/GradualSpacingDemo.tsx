b942aa4edf774558dd145ba56cc28310e98ba509
briskaputraa <briskaananda103@gmail.com>
2024-07-11 13:36:36 +0700

    Name

--- src/app/components/ui/GradualSpacingDemo.tsx
+++ src/app/components/ui/GradualSpacingDemo.tsx
@@ -4,7 +4,7 @@ export async function GradualSpacingDemo() {
   return (
     <GradualSpacing
       className="font-poppins tracking-tighter text-4xl font-semibold text-slate-50 md:text-5xl"
-      text="Hi, Briska is here."
+      text="Briska Ananda"
     />
   );
 }
